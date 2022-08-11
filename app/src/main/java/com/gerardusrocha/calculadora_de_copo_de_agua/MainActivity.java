ae82aa5f930919d0330ff2a62fca29d1f634d5a6
Gerabtc <rochagerardus1977@hotmail.com>
2022-08-11 13:22:27 -0300

    Criado a activity main e a mainActivity

---
+++ app/src/main/java/com/gerardusrocha/calculadora_de_copo_de_agua/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gerardusrocha.calculadora_de_copo_de_agua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
