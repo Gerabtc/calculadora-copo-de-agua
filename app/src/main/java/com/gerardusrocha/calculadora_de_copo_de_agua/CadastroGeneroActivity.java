f59ca6f62d7f56e11d53fc45636fd307c6793e64
Gerabtc <rochagerardus1977@hotmail.com>
2022-08-12 00:57:30 -0300

    Criaçao da activity cadastro gênero e o seu arquivo xml, adicionado as imagens ao arquivo drawable, elipse azul, elipse cinza, hora de dormir, peso branco, relogio branco, seta azul, seta branca e simbolo sexo branco

---
+++ app/src/main/java/com/gerardusrocha/calculadora_de_copo_de_agua/CadastroGeneroActivity.java
@@ -0,0 +1,13 @@
+package com.gerardusrocha.calculadora_de_copo_de_agua;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroGeneroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_genero);
+    }
+}
--- app/src/main/java/com/gerardusrocha/calculadora_de_copo_de_agua/MainActivity.java
+++ app/src/main/java/com/gerardusrocha/calculadora_de_copo_de_agua/MainActivity.java
@@ -2,13 +2,32 @@ package com.gerardusrocha.calculadora_de_copo_de_agua;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonVamos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonVamos = findViewById(R.id.buttonVamos);
+
+        buttonVamos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), CadastroGeneroActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
